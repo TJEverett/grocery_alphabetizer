67cef5f5112378a6e53691983aee7f906c31cbc6
Tristen Everett <spock214@gmail.com>
2020-04-02 11:57:38 -0700

    add global variable of how many items are in the form, and form functions for both buttons with comments instead of code for what they should end up doing

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+var listSize = 2;
+
+
+
+
+
+
+
+
+
+jQuery(document).ready(function(){
+  jQuery("#groceryForm").button(function(event){
+    event.preventDefault();
+    // add new list item
+  });
+
+  jQuery("#groceryForm").submit(function(event){
+    event.preventDefault();
+    // alphabetize list and return
+  });
+
+
+
+
+
+});
