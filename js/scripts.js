a8462c35ce0802ff09192e0cc93831dec9eb97c8
Tristen Everett <spock214@gmail.com>
2020-04-03 11:05:48 -0700

    add buisness logic to check if form is empty and to add new item entries to the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,42 @@
 var listSize = 2;
+var listIds = ["item1", "item2"];
 
+var emptyCheck = function(){
+  var test = "item";
+  var result = "full";
+  listIds.forEach(function(listitem){
+    test = jQuery("input#" + listitem).val();
+    if (test === ""){
+      result = "empty";
+    };
+  });
+  if (result = "empty"){
+    return true;
+  } else if (result = "full"){
+    return false;
+  }
+};
 
-
-
-
+var addItem = function(){
+  listSize = listSize + 1;
+  listIds.push("item" + listSize);
+  var newItem = '<div class="form-group"><label for="' + listIds[listSize] + '">Item ' + listSize + ':</label><input type="text" class="form-control" id="' + listIds[listSize] + '"></div>';
+  jQuery(".items").append(newItem);
+};
 
 
 
 
 jQuery(document).ready(function(){
-  jQuery("#groceryForm").button(function(event){
+  jQuery("#new").click(function(event){
     event.preventDefault();
+    addItem();
     // add new list item
-  });
+  })
 
   jQuery("#groceryForm").submit(function(event){
     event.preventDefault();
+    var empty = emptyCheck();
     // alphabetize list and return
   });
 
