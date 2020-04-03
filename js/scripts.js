092c9fd9075c1b1a574d137987a22fa560853350
Tristen Everett <spock214@gmail.com>
2020-04-03 12:02:01 -0700

    fix mistake in buisness logic, and make submit button print an alphabetized list of the user inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 var listSize = 2;
 var listIds = ["item1", "item2"];
+var listItems = [];
 
 var emptyCheck = function(){
   var test = "item";
   var result = "full";
-  listIds.forEach(function(listitem){
-    test = jQuery("input#" + listitem).val();
+  listIds.forEach(function(listItem){
+    test = jQuery("input#" + listItem).val();
+    console.log(test);
     if (test === ""){
+      console.log(true)
       result = "empty";
     };
   });
-  if (result = "empty"){
+  if (result === "empty"){
     return true;
-  } else if (result = "full"){
+  } else if (result === "full"){
     return false;
   }
 };
@@ -20,24 +23,32 @@ var emptyCheck = function(){
 var addItem = function(){
   listSize = listSize + 1;
   listIds.push("item" + listSize);
-  var newItem = '<div class="form-group"><label for="' + listIds[listSize] + '">Item ' + listSize + ':</label><input type="text" class="form-control" id="' + listIds[listSize] + '"></div>';
+  var newId = listIds[listSize - 1]
+  var newItem = '<div class="form-group"><label for="' + newId + '">Item ' + listSize + ':</label><input type="text" class="form-control" id="' + newId + '"></div>';
   jQuery(".items").append(newItem);
 };
-
-
-
 
 jQuery(document).ready(function(){
   jQuery("#new").click(function(event){
     event.preventDefault();
     addItem();
-    // add new list item
   })
 
   jQuery("#groceryForm").submit(function(event){
     event.preventDefault();
     var empty = emptyCheck();
-    // alphabetize list and return
+    if (empty === false){
+      listIds.forEach(function(id){
+        var listItem = jQuery("input#" + id).val();
+        listItems.push(listItem);
+      });
+      listItems.sort();
+      listItems.forEach(function(item){
+        jQuery(".listReturn ul").append("<li>" + item + "</li>");
+      });
+    } else {
+      alert("Please fill in all items");
+    };
   });
 
 
